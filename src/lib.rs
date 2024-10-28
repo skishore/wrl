6e132c927d851ce74284aa45f0f695e28ff7a693
Shaunak Kishore <kshaunak@gmail.com>
2024-10-28 14:10:38 -0400

    PIXI for rendering

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use std::cell::RefCell;
 use wasm_bindgen::prelude::*;
 
 use base::{Glyph, Matrix};
-use game::State;
+use game::{Input, State};
 
 thread_local! {
     static STATE: RefCell<State> = State::new(None).into();
@@ -23,6 +23,18 @@ thread_local! {
 #[wasm_bindgen(module = "/bindings.js")]
 extern "C" {
     fn render(ptr: *const Glyph, sx: i32, sy: i32) -> i32;
+}
+
+#[wasm_bindgen]
+pub fn keydown(ch: i32, shift: bool) {
+    let input = if ch == 27 {
+        Input::Escape
+    } else if ch == 9 && shift {
+        Input::BackTab
+    } else {
+        Input::Char(ch as u8 as char)
+    };
+    STATE.with_borrow_mut(|game|{ game.add_input(input); });
 }
 
 #[wasm_bindgen]
