202761141a30998b6d62887b2fe245b138802f6c
Shaunak Kishore <kshaunak@gmail.com>
2025-05-23 17:03:00 -0400

    Prototype minigame

--- base/src/game.rs
+++ base/src/game.rs
@@ -23,7 +23,7 @@ use crate::ui::{UI, get_direction};
 
 pub const MOVE_TIMER: i32 = 960;
 pub const TURN_TIMER: i32 = 120;
-pub const WORLD_SIZE: i32 = 50;
+pub const WORLD_SIZE: i32 = 100;
 
 pub const FOV_RADIUS_NPC: i32 = 12;
 pub const FOV_RADIUS_PC_: i32 = 21;
@@ -36,8 +36,8 @@ const SPEED_NPC: f64 = 0.1;
 
 const LIGHT: Light = Light::Sun(Point(4, 1));
 const WEATHER: Weather = Weather::None;
-const NUM_PREDATORS: i32 = 4;
-const NUM_PREY: i32 = 1;
+const NUM_PREDATORS: i32 = 20;
+const NUM_PREY: i32 = 0;
 
 const UI_DAMAGE_FLASH: i32 = 6;
 const UI_DAMAGE_TICKS: i32 = 6;
