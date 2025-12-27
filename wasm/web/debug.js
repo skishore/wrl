518fbe383641e654ea890c1189b8ac6551dd7b16
Shaunak Kishore <kshaunak@gmail.com>
2025-12-26 20:18:12 -0500

    Clean up debug UI dirty flags

--- wasm/web/debug.js
+++ wasm/web/debug.js
@@ -8,13 +8,16 @@ class DebugTrace {
     this.eid = '';
     this.index = 0;
     this.ticks = [];
-    this.entities = [];
-    this.aiOutput = [];
-    this.lastIndex = -1;
-    this.lastTicks = '';
+    this.tickState = {
+      aiOutput: [],
+      entities: [],
+      map: [],
+    };
     this.showAllEntities = true;
 
     this.reloading = false;
+    this.lastIndex = -1;
+    this.lastTicks = '';
 
     this.terminal = new Terminal();
     this.ui = {
@@ -36,7 +39,7 @@ class DebugTrace {
 
   onShowAllEntitiesChange() {
     this.showAllEntities = this.ui.checkbox.checked;
-    this.lastIndex = -1;
+    this.markDirty();
   }
 
   onkeydown(keyEvent) {
@@ -58,9 +61,8 @@ class DebugTrace {
     const next = key === 'j' ? prev + 1 : key === 'k' ? prev - 1 : -1;
     if (next < 0 || next >= options.length) return;
 
-    this.ui.timeline.children[prev].classList.remove('highlighted');
-    this.ui.timeline.children[next].classList.add('highlighted');
     this.index = options[next][1];
+    this.markDirty();
   }
 
 
@@ -69,15 +71,14 @@ class DebugTrace {
     if (eid === null) return;
 
     this.eid = eid;
-    this.lastIndex = -1;
-    this.lastTicks = '';
+    this.markDirty();
   }
 
   onmousemove(mouseEvent) {
     const eid = this.getEID(mouseEvent);
     const id = eid ? `entity-${eid}` : '';
 
-    for (const entity of this.entities) {
+    for (const entity of this.tickState.entities) {
       for (const element of this.ui.entities.children) {
         if (element.id !== id) element.classList.remove('mouseover');
         if (element.id === id) element.classList.add('mouseover');
@@ -90,7 +91,7 @@ class DebugTrace {
     const x = skip ? -1 : Math.floor(mouseEvent.layerX / (2 * this.terminal.unitX));
     const y = skip ? -1 : Math.floor(mouseEvent.layerY / this.terminal.unitY);
 
-    const entity = this.entities.filter(e => e.posX === x && e.posY === y)[0];
+    const entity = this.tickState.entities.filter(e => e.posX === x && e.posY === y)[0];
     return entity ? entity.eid : null;
   }
 
@@ -104,25 +105,28 @@ class DebugTrace {
     if (this.reloading) return;
     this.reloading = true;
     try {
-      await this.reloadTimeline();
-      await this.reloadView();
+      await this.reloadTicks();
+      await this.reloadCurrentTick();
+      this.redraw();
     } catch (e) {
       console.error(e);
     }
     this.reloading = false;
   }
 
-  async reloadTimeline() {
+  async reloadTicks() {
     const response = await fetch('debug/ticks.txt');
     const ticks = await response.text();
-    if (ticks === this.lastTicks) return;
+    if (ticks == this.lastTicks) return;
 
-    this.lastTicks = ticks;
     this.ticks = [];
     for (const line of ticks.trim().split('\n')) {
       try { this.ticks.push(JSON.parse(line)); } catch { break; }
     }
     this.index = Math.max(0, Math.min(this.index, this.ticks.length - 1));
+    this.lastTicks = ticks;
+    this.dirty = true;
+
     if (this.ticks.length === 0) return;
 
     const options = this.ticks.map((x, i) => [x, i]).filter(x => x[0].eid === this.eid);
@@ -132,6 +136,113 @@ class DebugTrace {
       let best = options.filter(x => x[1] >= this.index)[0];
       best = best ? best : options[options.length - 1];
       this.index = best[1];
+    }
+  }
+
+  async reloadCurrentTick() {
+    if (this.index === this.lastIndex) return;
+
+    const response = await fetch(`debug/tick-${this.index}.bin.gz`);
+    const inflated = new Response(response.body.pipeThrough(new DecompressionStream('gzip')));
+    const data = await inflated.arrayBuffer();
+    const reader = new Reader(data);
+
+    this.lastIndex = this.index;
+    this.dirty = true;
+
+    const numEntities = reader.readInt();
+    this.tickState.entities.length = 0;
+    for (let i = 0; i < numEntities; i++) {
+      const eid = reader.readStr();
+      const name = reader.readStr();
+      const health = reader.readDbl();
+      const posX = reader.readInt();
+      const posY = reader.readInt();
+      const glyph0 = reader.readInt();
+      const glyph1 = reader.readInt();
+      this.tickState.entities.push({eid, name, health, posX, posY, glyph0, glyph1});
+    }
+
+    const numLines = reader.readInt();
+    this.tickState.aiOutput.length = 0;
+    for (let i = 0; i < numLines; i++) {
+      const color = reader.readInt();
+      const depth = reader.readInt();
+      const text = reader.readStr();
+      this.tickState.aiOutput.push({color, depth, text});
+    }
+
+    const w = reader.readInt();
+    const h = reader.readInt();
+    if (w !== this.mapX || h !== this.mapY) {
+      throw Error(`Expected: ${this.mapX} x ${this.mapY} map; got: ${w} x ${h}`);
+    }
+    this.tickState.map.length = 0;
+    for (let i = 0; i < 2 * w * h; i++) {
+      this.tickState.map.push(reader.readInt());
+    }
+  }
+
+  markDirty() {
+    this.lastTicks = '';
+  }
+
+  redraw() {
+    if (!this.dirty) return;
+    this.dirty = false;
+
+    this.redrawAITrace();
+    this.redrawEntities();
+    this.redrawMap();
+    this.redrawTimeline();
+  }
+
+  redrawAITrace() {
+    const traceElements = this.tickState.aiOutput.map(x => {
+      const element = document.createElement('div');
+      const color = x.color.toString(16);
+      const zeros = '0'.repeat(6 - color.length);
+      element.textContent = `${x.text}`;
+      element.classList.add('ai-trace-line');
+      element.style = `color: #${zeros}${color}; margin-left: ${12 * x.depth}px`;
+      return element;
+    });
+    this.ui.aiOutput.replaceChildren(...traceElements);
+  }
+
+  redrawEntities() {
+    const entityElements = this.tickState.entities.map(x => {
+      const element = document.createElement('div');
+      element.id = `entity-${x.eid}`;
+      element.textContent = `${x.name} - ${Math.max(Math.floor(100 * x.health), 1)}%`;
+      element.classList.add('entity');
+      if (x.eid === this.eid) element.classList.add('highlighted');
+      return element;
+    });
+    this.ui.entities.replaceChildren(...entityElements);
+  }
+
+  redrawMap() {
+    let index = 0;
+    const map = this.tickState.map;
+    for (let y = 0; y < this.mapY; y++) {
+      for (let x = 0; x < this.mapX; x++) {
+        this.terminal.drawGlyph(2 * x, y, map[index], map[index + 1]);
+        index += 2;
+      }
+    }
+    if (this.showAllEntities) {
+      for (const entity of this.tickState.entities) {
+        const {posX, posY, glyph0, glyph1} = entity;
+        this.terminal.drawGlyph(2 * posX, posY, glyph0, glyph1);
+      }
+    }
+  }
+
+  redrawTimeline() {
+    if (this.ticks.length === 0) {
+      this.ui.timeline.replaceChildren();
+      return;
     }
 
     const min = BigInt(this.ticks[0].time);
@@ -152,77 +263,6 @@ class DebugTrace {
       return element;
     });
     this.ui.timeline.replaceChildren(...elements.filter(x => !!x));
-  }
-
-  async reloadView() {
-    if (this.index === this.lastIndex) return;
-
-    this.lastIndex = this.index;
-    const response = await fetch(`debug/tick-${this.index}.bin.gz`);
-    const inflated = new Response(response.body.pipeThrough(new DecompressionStream('gzip')));
-    const data = await inflated.arrayBuffer();
-    const reader = new Reader(data);
-
-    const numEntities = reader.readInt();
-    this.entities.length = 0;
-    for (let i = 0; i < numEntities; i++) {
-      const eid = reader.readStr();
-      const name = reader.readStr();
-      const health = reader.readDbl();
-      const posX = reader.readInt();
-      const posY = reader.readInt();
-      const glyph0 = reader.readInt();
-      const glyph1 = reader.readInt();
-      this.entities.push({eid, name, health, posX, posY, glyph0, glyph1});
-    }
-
-    const entityElements = this.entities.map(x => {
-      const element = document.createElement('div');
-      element.id = `entity-${x.eid}`;
-      element.textContent = `${x.name} - ${Math.max(Math.floor(100 * x.health), 1)}%`;
-      element.classList.add('entity');
-      if (x.eid === this.eid) element.classList.add('highlighted');
-      return element;
-    });
-    this.ui.entities.replaceChildren(...entityElements);
-
-    const numLines = reader.readInt();
-    this.aiOutput.length = 0;
-    for (let i = 0; i < numLines; i++) {
-      const color = reader.readInt();
-      const depth = reader.readInt();
-      const text = reader.readStr();
-      this.aiOutput.push({color, depth, text});
-    }
-
-    const traceElements = this.aiOutput.map(x => {
-      const element = document.createElement('div');
-      const color = x.color.toString(16);
-      const zeros = '0'.repeat(6 - color.length);
-      element.textContent = `${x.text}`;
-      element.classList.add('ai-trace-line');
-      element.style = `color: #${zeros}${color}; margin-left: ${12 * x.depth}px`;
-      return element;
-    });
-    this.ui.aiOutput.replaceChildren(...traceElements);
-
-    const w = reader.readInt();
-    const h = reader.readInt();
-    if (w !== this.mapX || h !== this.mapY) {
-      throw Error(`Expected: ${this.mapX} x ${this.mapY} map; got: ${w} x ${h}`);
-    }
-
-    for (let y = 0; y < this.mapY; y++) {
-      for (let x = 0; x < this.mapX; x++) {
-        this.terminal.draw(2 * x, y, reader);
-      }
-    }
-    if (this.showAllEntities) {
-      for (const entity of this.entities) {
-        const {posX, posY, glyph0, glyph1} = entity;
-        this.terminal.drawGlyph(2 * posX, posY, glyph0, glyph1);
-      }
-    }
   }
 }
 
